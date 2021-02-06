e56b0ff17f948c8c0861b79e42dfbc21a5ea22ff
wu-kan <i@wu-kan.cn>
2021-02-06 19:12:49 +0800

    add hplai_comm.h

--- include/hplai_comm.h
+++ include/hplai_comm.h
@@ -30,6 +30,61 @@ STDC_ARGS( (
    int,
    MPI_Comm
 ) );
+int                              HPLAI_sdrv
+STDC_ARGS( (
+   HPLAI_T_AFLOAT *,
+   int,
+   int,
+   HPLAI_T_AFLOAT *,
+   int,
+   int,
+   int,
+   MPI_Comm
+) );
+int                              HPLAI_binit
+STDC_ARGS( (
+   HPLAI_T_panel *
+) );
+int                              HPLAI_bcast
+STDC_ARGS( (
+   HPLAI_T_panel *,
+   int *
+) );
+int                              HPLAI_bwait
+STDC_ARGS( (
+   HPLAI_T_panel *
+) );
+int                              HPLAI_packL
+STDC_ARGS( (
+   HPLAI_T_panel *,
+   const int,
+   const int,
+   const int
+) );
+ 
+int HPLAI_binit_1ring STDC_ARGS( ( HPLAI_T_panel *        ) );
+int HPLAI_bcast_1ring STDC_ARGS( ( HPLAI_T_panel *, int * ) );
+int HPLAI_bwait_1ring STDC_ARGS( ( HPLAI_T_panel *        ) );
+ 
+int HPLAI_binit_1rinM STDC_ARGS( ( HPLAI_T_panel *        ) );
+int HPLAI_bcast_1rinM STDC_ARGS( ( HPLAI_T_panel *, int * ) );
+int HPLAI_bwait_1rinM STDC_ARGS( ( HPLAI_T_panel *        ) );
+ 
+int HPLAI_binit_2ring STDC_ARGS( ( HPLAI_T_panel *        ) );
+int HPLAI_bcast_2ring STDC_ARGS( ( HPLAI_T_panel *, int * ) );
+int HPLAI_bwait_2ring STDC_ARGS( ( HPLAI_T_panel *        ) );
+ 
+int HPLAI_binit_2rinM STDC_ARGS( ( HPLAI_T_panel *        ) );
+int HPLAI_bcast_2rinM STDC_ARGS( ( HPLAI_T_panel *, int * ) );
+int HPLAI_bwait_2rinM STDC_ARGS( ( HPLAI_T_panel *        ) );
+ 
+int HPLAI_binit_blong STDC_ARGS( ( HPLAI_T_panel *        ) );
+int HPLAI_bcast_blong STDC_ARGS( ( HPLAI_T_panel *, int * ) );
+int HPLAI_bwait_blong STDC_ARGS( ( HPLAI_T_panel *        ) );
+ 
+int HPLAI_binit_blonM STDC_ARGS( ( HPLAI_T_panel *        ) );
+int HPLAI_bcast_blonM STDC_ARGS( ( HPLAI_T_panel *, int * ) );
+int HPLAI_bwait_blonM STDC_ARGS( ( HPLAI_T_panel *        ) );
 
 #endif
 /*
