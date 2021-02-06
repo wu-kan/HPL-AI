7734e20aeb3d7f44a299c1b40a89e1a9b649a2c9
wu-kan <i@wu-kan.cn>
2021-02-06 17:25:10 +0800

    更新测试脚本

--- run.th2k.work.sh
+++ run.th2k.work.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+source ../spack/share/spack/setup-env.sh
+spack unload -a
+spack load gcc@7.5.0%gcc@4.8.5
+spack load autoconf@2.69%gcc@7.5.0
+spack load autoconf-archive@2019.01.06%gcc@7.5.0
+spack load automake@1.16.3%gcc@7.5.0
+spack load openmpi@4.0.5%gcc@7.5.0
+spack load openblas@0.3.13%gcc@7.5.0
 aclocal
 autoconf
 autoheader
