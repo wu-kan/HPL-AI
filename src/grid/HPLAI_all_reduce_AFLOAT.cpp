de38d03f98b19df3e4b6d192ebcd18e23b721afa
wu-kan <i@wu-kan.cn>
2021-02-07 19:52:04 +0800

    add HPLAI_all_reduce_AFLOAT

--- src/grid/HPLAI_all_reduce_AFLOAT.cpp
+++ src/grid/HPLAI_all_reduce_AFLOAT.cpp
@@ -1,70 +1,27 @@
-/* 
- * -- High Performance Computing Linpack Benchmark (HPL)                
- *    HPL - 2.3 - December 2, 2018                          
- *    Antoine P. Petitet                                                
- *    University of Tennessee, Knoxville                                
- *    Innovative Computing Laboratory                                 
- *    (C) Copyright 2000-2008 All Rights Reserved                       
- *                                                                      
- * -- Copyright notice and Licensing terms:                             
- *                                                                      
- * Redistribution  and  use in  source and binary forms, with or without
- * modification, are  permitted provided  that the following  conditions
- * are met:                                                             
- *                                                                      
- * 1. Redistributions  of  source  code  must retain the above copyright
- * notice, this list of conditions and the following disclaimer.        
- *                                                                      
- * 2. Redistributions in binary form must reproduce  the above copyright
- * notice, this list of conditions,  and the following disclaimer in the
- * documentation and/or other materials provided with the distribution. 
- *                                                                      
- * 3. All  advertising  materials  mentioning  features  or  use of this
- * software must display the following acknowledgement:                 
- * This  product  includes  software  developed  at  the  University  of
- * Tennessee, Knoxville, Innovative Computing Laboratory.             
- *                                                                      
- * 4. The name of the  University,  the name of the  Laboratory,  or the
- * names  of  its  contributors  may  not  be used to endorse or promote
- * products  derived   from   this  software  without  specific  written
- * permission.                                                          
- *                                                                      
- * -- Disclaimer:                                                       
- *                                                                      
- * THIS  SOFTWARE  IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,  INCLUDING,  BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE UNIVERSITY
- * OR  CONTRIBUTORS  BE  LIABLE FOR ANY  DIRECT,  INDIRECT,  INCIDENTAL,
- * SPECIAL,  EXEMPLARY,  OR  CONSEQUENTIAL DAMAGES  (INCLUDING,  BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA OR PROFITS; OR BUSINESS INTERRUPTION)  HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT,  STRICT LIABILITY,  OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
- * ---------------------------------------------------------------------
- */ 
 /*
  * Include files
  */
 #include "hplai.h"
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #ifdef STDC_HEADERS
-int HPL_all_reduce
+int HPLAI_all_reduce_AFLOAT
 (
    void *                           BUFFER,
    const int                        COUNT,
-   const HPL_T_TYPE                 DTYPE,
-   const HPL_T_OP                   OP,
+   const HPLAI_T_OP_AFLOAT          OP,
    MPI_Comm                         COMM
 )
 #else
-int HPL_all_reduce
+int HPLAI_all_reduce_AFLOAT
 ( BUFFER, COUNT, DTYPE, OP, COMM )
    void *                           BUFFER;
    const int                        COUNT;
-   const HPL_T_TYPE                 DTYPE;
-   const HPL_T_OP                   OP;
+   const HPLAI_T_OP_AFLOAT          OP;
    MPI_Comm                         COMM;
 #endif
 {
@@ -72,7 +29,7 @@ int HPL_all_reduce
  * Purpose
  * =======
  *
- * HPL_all_reduce performs   a   global   reduce  operation  across  all
+ * HPLAI_all_reduce_AFLOAT performs   a   global   reduce  operation  across  all
  * processes of a group leaving the results on all processes.
  *
  * Arguments
@@ -87,10 +44,7 @@ int HPL_all_reduce
  *         On entry,  COUNT  indicates the number of entries in  BUFFER.
  *         COUNT must be at least zero.
  *
- * DTYPE   (global input)                const HPL_T_TYPE
- *         On entry,  DTYPE  specifies the type of the buffers operands.
- *
- * OP      (global input)                const HPL_T_OP 
+ * OP      (global input)                const HPLAI_T_OP_AFLOAT 
  *         On entry, OP is a pointer to the local combine function.
  *
  * COMM    (global/local input)          MPI_Comm
@@ -105,10 +59,14 @@ int HPL_all_reduce
 /* ..
  * .. Executable Statements ..
  */
-   hplerr = HPL_reduce(   BUFFER, COUNT, DTYPE, OP, 0, COMM );
+   hplerr = HPLAI_reduce_AFLOAT(   BUFFER, COUNT, OP, 0, COMM );
    if( hplerr != MPI_SUCCESS ) return( hplerr );
-   return( HPL_broadcast( BUFFER, COUNT, DTYPE,     0, COMM ) );
+   return( HPLAI_broadcast_AFLOAT( BUFFER, COUNT,     0, COMM ) );
 /*
- * End of HPL_all_reduce
+ * End of HPLAI_all_reduce_AFLOAT
  */
 }
+
+#ifdef __cplusplus
+}
+#endif
