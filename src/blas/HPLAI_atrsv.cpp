5f943755a4b1ad59f78435b6e39d946103a3eba8
wu-kan <i@wu-kan.cn>
2021-02-18 17:40:27 +0800

    add blas::trsv

--- src/blas/HPLAI_atrsv.cpp
+++ src/blas/HPLAI_atrsv.cpp
@@ -3,49 +3,29 @@
  */
 #include "hplai.h"
 
-template <typename T>
-static void HPLAI_atrsv_template(
-	const enum HPLAI_ORDER ORDER,
-	const enum HPLAI_UPLO UPLO,
-	const enum HPLAI_TRANS TRANS,
-	const enum HPLAI_DIAG DIAG,
-	const int N,
-	const T *A,
-	const int LDA,
-	T *X,
-	const int INCX);
-
 template <>
-void HPLAI_atrsv_template<double>(
-	const enum HPLAI_ORDER ORDER,
-	const enum HPLAI_UPLO UPLO,
-	const enum HPLAI_TRANS TRANS,
-	const enum HPLAI_DIAG DIAG,
-	const int N,
-	const double *A,
-	const int LDA,
-	double *X,
-	const int INCX)
+void blas::trsv<double>(
+	blas::Layout layout,
+	blas::Uplo uplo,
+	blas::Op trans,
+	blas::Diag diag,
+	int64_t n,
+	double const *A,
+	int64_t lda,
+	double *x,
+	int64_t incx)
 {
-	HPL_dtrsv(ORDER, UPLO, TRANS, DIAG, N, A, LDA, X, INCX);
+	HPL_dtrsv(
+		layout == blas::Layout::RowMajor ? HplRowMajor : HplColumnMajor,
+		uplo == blas::Uplo::Upper ? HplUpper : HplLower,
+		trans == blas::Op::Trans ? HplTrans : HplNoTrans,
+		diag == blas::Diag::Unit ? HplUnit : HplNonUnit,
+		n,
+		A,
+		lda,
+		x,
+		incx);
 }
-
-#if (HPL_CALL_CBLAS)
-template <>
-void HPLAI_atrsv_template<float>(
-	const enum HPLAI_ORDER ORDER,
-	const enum HPLAI_UPLO UPLO,
-	const enum HPLAI_TRANS TRANS,
-	const enum HPLAI_DIAG DIAG,
-	const int N,
-	const float *A,
-	const int LDA,
-	float *X,
-	const int INCX)
-{
-	cblas_strsv(ORDER, UPLO, TRANS, DIAG, N, A, LDA, X, INCX);
-}
-#endif
 
 #ifdef __cplusplus
 extern "C"
@@ -76,7 +56,12 @@ HPLAI_T_AFLOAT *X;
 const int INCX;
 #endif
 	{
-		HPLAI_atrsv_template(ORDER, UPLO, TRANS, DIAG, N, A, LDA, X, INCX);
+		blas::trsv(
+			ORDER == HPLAI_RowMajor ? blas::Layout::RowMajor : blas::Layout::ColMajor,
+			UPLO == HPLAI_Upper ? blas::Uplo::Upper : blas::Uplo::Lower,
+			TRANS == HPLAI_Trans ? blas::Op::Trans : blas::Op::NoTrans,
+			DIAG == HPLAI_Unit ? blas::Diag::Unit : blas::Diag::NonUnit,
+			N, A, LDA, X, INCX);
 		/*
 		 * End of HPLAI_atrsv
  		 */
