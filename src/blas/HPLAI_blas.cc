68ba46c3bbd79437cc355cff3ae30c532115ef2e
wu-kan <i@wu-kan.cn>
2021-04-18 08:15:49 +0800

    v2.3c

--- src/blas/HPLAI_blas.cc
+++ src/blas/HPLAI_blas.cc
@@ -223,19 +223,19 @@ void blas::gemm<HPLAI_T_AFLOAT, HPLAI_T_AFLOAT, HPLAI_T_AFLOAT>(
     int64_t padding_size = 128 / sizeof(HPLAI_T_AFLOAT);
     if (padding_size < 1)
         padding_size = 1;
-    
+
     blas::Op TRANSC = blas::Op::NoTrans;
-    
+
     int64_t rC = TRANSC == blas::Op::NoTrans ? M : N;
     int64_t cC = TRANSC == blas::Op::NoTrans ? N : M;
     int64_t dLDC = (rC + padding_size - 1) / padding_size * padding_size;
     int64_t dsC = cC * dLDC;
-    
+
     int64_t rB = TRANSB == blas::Op::NoTrans ? K : N;
     int64_t cB = TRANSB == blas::Op::NoTrans ? N : K;
     int64_t dLDB = (rB + padding_size - 1) / padding_size * padding_size;
     int64_t dsB = cB * dLDB;
-    
+
     int64_t rA = TRANSA == blas::Op::NoTrans ? M : K;
     int64_t cA = TRANSA == blas::Op::NoTrans ? K : M;
     int64_t dLDA = (rA + padding_size - 1) / padding_size * padding_size;
